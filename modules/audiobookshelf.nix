8fa965a5723a23b69c6a60f352926d3eee1fe3d6
kryt <krytsnooker@gmail.com>
2026-09-15 16:10:09 +1000

    audiobookshelf: pin image digest

--- modules/audiobookshelf.nix
+++ modules/audiobookshelf.nix
@@ -2,7 +2,7 @@
 
 {
   virtualisation.oci-containers.containers.audiobookshelf = {
-    image = "ghcr.io/advplyr/audiobookshelf:latest";
+    image = "ghcr.io/advplyr/audiobookshelf@sha256:e388e90e381ae3fa8660346612b2955f2c555ede81c9c286e2218bdf966b4de8";
     ports = [ "13378:80/tcp" ];
     volumes = [
       "/var/lib/audiobookshelf/config:/config"
